0d2546fa76300f870dc00977691b37b877e9bc5e
Eric Baukhages <febvigrail@gmail.com>
2020-03-30 01:40:16 -0400

    Add PostCSS

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,28 @@ const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
   mode: process.env.NODE_ENV === "production" ? "production" : "development",
-  entry: "./src/js/index.js",
+  entry: ["./src/js/index.js", "./src/css/styles.css"],
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+            }
+          },
+          {
+            loader: "postcss-loader",
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({ template: "./src/index.html" }),
